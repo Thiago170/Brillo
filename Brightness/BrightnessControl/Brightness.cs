f9fdde4b7c0f53af0ce9c118773108d7c1d33c47
Thiago170 <91340096+Thiago170@users.noreply.github.com>
2021-12-09 17:34:10 -0300

    Add files via upload

---
+++ Brightness/BrightnessControl/Brightness.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BrightnessControl
+{
+  public partial class Brightness : Form
+  {
+
+    public Program program;
+    public Brightness()
+    {
+      InitializeComponent();
+    }
+    private void ScreenForm_FormClosed(object sender, FormClosedEventArgs e)
+    {
+      program.ExitThread();
+    }
+    protected override void WndProc(ref Message m)
+    {
+      if (m.Msg == 0x0084) // WM_NCHITTEST
+        m.Result = (IntPtr)(-1); // HTTRANSPARENT
+      else
+        base.WndProc(ref m);
+    }
+  }
+}
---
+++ Brightness/BrightnessControl/ControlForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BrightnessControl
+{
+  public partial class ControlForm : Form
+  {
+    public Program program;
+    public ControlForm()
+    {
+      InitializeComponent();
+    }
+  
+    void UpdateBrightness()
+    {
+      float f = trackBarBrightness.Value * 0.01f;
+      if (f < 0.5f)
+      {
+        program.screenForm.Opacity = 1 - 2 * f;
+        program.screenForm.BackColor = Color.Black;
+      }
+      else
+      {
+        program.screenForm.Opacity = 2 * (f - 0.5f);
+        program.screenForm.BackColor = Color.White;
+      }
+    }
+    private void brightnessBar_ValueChanged(object sender, EventArgs e)
+    {
+      UpdateBrightness();
+    }
+
+    private void ControlForm_FormClosed_1(object sender, FormClosedEventArgs e)
+    {
+      program.ExitThread();
+    }
+
+    private void ControlForm_Load(object sender, EventArgs e)
+    {
+      UpdateBrightness();
+    }
+  }
+}
